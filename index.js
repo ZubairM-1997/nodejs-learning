136f2eff49c7d0f40c54ccf3da0c9c4a69cf0c50
Zubair <zubair.maqsood6@gmail.com>
2019-11-17 01:33:10 +0000

    more express

--- index.js
+++ index.js
@@ -1,21 +1,32 @@
 // const http = require("http")
 
 const express = require("express")
+const bodyParser = require("body-parser")
 
 const app = express();
 
-
+app.use(bodyParser.urlencoded());
 //allows us to add a middleware function
-app.use((req, res, next) => {
-	console.log("In the middleware")
+app.use('/',(req, res, next) => {
+	console.log("This always runs!")
 	next() //Allows the request to continue to the next middleware in line
 
 });
 
-app.use((req, res, next) => {
+app.use('/add-product',(req, res, next) => {
 	console.log("In the middleware")
-	res.send('<h1>Hello from Express<h1>');
+	res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit" />');
 });
+
+app.post("/product", (req, res, next) => {
+	console.log(req.body)
+	res.redirect("/")
+})
+
+app.use("/", (req, res, next) => {
+	console.log("In another middleware")
+	res.send("<h1> Hello from express </h1>")
+})
 
 
 
